03b417ca47cbdc0c78910bcbd6baad3ee2de7dcc
junshen2110 <143473514+junshen2110@users.noreply.github.com>
2023-09-22 22:54:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "student.ctktvos05fp8.us-east-1.rds.amazonaws.com"
 customuser = "studentASM"
 custompass = "studentASM"
-customdb = "Student"
+customdb = "student"
 custombucket = "tehjunshen-bucket"
 customregion = "us-east-1"
 
